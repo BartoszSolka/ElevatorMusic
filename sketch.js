1b5893f0ea4c5a77755245ac0f758cd098910b99
Bartosz Solka <batisol13@gmail.com>
2017-05-17 10:27:39 +0200

    Obsługa listy wezwań

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var speedMultiplier = 1;
+var speedMultiplier = 2;
 var floorHeight = 40;
 var floorCount = 10;
 var machineRoomHeight = 30;
@@ -10,11 +10,18 @@ var canvasWidth = 1024;
 var bottomLevel = canvasHeight - 1;
 var padding = 100;
 var elevator;
+var input;
+var button;
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 function Elevator() {
 	var initFloor = getFloor(10);
 	this.currentFloor = initFloor;
 	this.goingTo = getFloor(1);
+	this.calls = [getFloor(10), getFloor(1)];
 	this.x = this.currentFloor.x;
 	this.y = this.currentFloor.y;
 	this.peopleCount = 0;
@@ -24,16 +31,21 @@ function Elevator() {
 		fill(245, 195, 66);
 		rect(this.x, this.y, elevatorWidth, floorHeight);
 		fill(255);
-		text(this.peopleCount, this.x + 5, this.y + floorHeight/2);
+		text(this.goingTo.floor, this.x + 5, this.y + floorHeight/2);
 	};
 
 	this.moveToFloor = function(floorNumber) {
-		this.goingTo = getFloor(floorNumber);
+		this.calls.push(getFloor(floorNumber));
+		console.log(floorNumber, this.calls, this.calls.length);
 	};
 
 	this.move = function() {
 		if (abs(this.y - this.goingTo.y) < 1) {
 			this.currentFloor = this.goingTo;
+			if (this.calls.length) {
+				this.goingTo = this.calls[0];
+				this.calls.shift();
+			}
 			return;
 		}
 		this.y += this.speed * getDirection(this.y, this.goingTo.y);
@@ -57,12 +69,27 @@ function drawFloors() {
 }
 
 function getFloor(floorNumber) {
+	if (floorNumber < 1) {
+		return getFloor(1);
+	} else if (floorNumber > floorCount) {
+		return getFloor(floorCount);
+	}
 	return {x: padding, y: bottomLevel - floorNumber * floorHeight, floor: floorNumber};
 }
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   elevator = new Elevator();
+
+  input = createInput();
+  input.position(520, 65);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(moveToFloor);
+
+  greeting = createElement('h2', 'Gdzie chcesz pojechac?');
+  greeting.position(520, 5);
 }
 
 function draw() {
@@ -71,4 +98,12 @@ function draw() {
   drawMachineRoom();
 	elevator.move();
   elevator.display();
+}
+
+function moveToFloor() {
+	var floorNumber = input.value();
+	input.value('');
+	if (isNumber(floorNumber)) {
+		elevator.moveToFloor(floorNumber);
+	}
 }
